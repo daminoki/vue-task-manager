07a9781a4cb24057161b573bcbfecf65c93c9fec
Daria Andreeva <dd1556@mail.ru>
2023-06-17 12:16:40 +0300

    add task list component

---
+++ src/components/TasksList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="tasks-list">
+    <li v-for="task in getTasks" :key="task.id">
+      <TaskItem :task="task" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import TaskItem from '@/components/TaskItem.vue';
+
+export default {
+  components: { TaskItem },
+  computed: {
+    ...mapGetters(['getTasks']),
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
