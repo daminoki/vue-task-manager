ae908892569247cbe3b3ec6873bf8936c9d5ad3f
Daria Andreeva <dd1556@mail.ru>
2023-06-17 12:16:28 +0300

    add task item component

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="task-item">
+    <label for="checkbox">
+      <input type="checkbox" name="checkbox" />
+    </label>
+    <p>{{  task.description  }}</p>
+    <button>Edit</button>
+    <button>Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
